36042799cd13063784c1d4b91b0634df3cd1c8db
ce-muzzamil <ce.muzzamil@gmail.com>
2025-06-24 20:35:43 +0300

    update

--- test.py
+++ test.py
@@ -350,7 +350,7 @@ def collect_rollout(env, model, rollout_len=2048):
         for lot in info_buf[i]["done_lots"]:
             for j in range(i):
                 if info_buf[j]["time"] == lot.tag:
-                    reward_buf[j] += max(lot.deadline_at - lot.done_at, 0)
+                    reward_buf[j] += max(lot.deadline_at - lot.done_at, 0)/60.
                     # reward_buf[j] += 10 if lot.deadline_at >= lot.done_at else 1
  
     print("counter: ", counter, "time:", {np.round(env.instance.current_time/3600/24, 3)})
